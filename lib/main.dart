08930df23a3a99b5b29873aa4fc3e5ec27d5d4a9
Yassine Mokni <thexlearner.webdev@gmail.com>
2019-07-10 00:14:42 +0100

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyHomePage(title: 'Money Watcher'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20.0),
+        crossAxisSpacing: 10.0,
+        crossAxisCount: 2,
+        children: <Widget>[
+          const Text('Fuel'),
+          const Text('Food'),
+          const Text('Family'),
+          const Text('Coffe'),
+        ],
+      ),
+    );
+  }
+}
